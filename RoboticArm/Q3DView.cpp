d021203e328dfeb5270d81dcb0d2bd7773f1a220
Alexandre Desgagné <alexandre1998@live.ca>
2020-03-29 20:07:26 -0400

    Import arm waist.

--- RoboticArm/Q3DView.cpp
+++ RoboticArm/Q3DView.cpp
@@ -2,6 +2,7 @@
 
 #include <Qt3DExtras/QOrbitCameraController>
 #include <Qt3DCore/QEntity>
+#include <Qt3DCore/QTransform>
 #include <Qt3DExtras/QTorusMesh>
 #include <Qt3DRender/QCamera>
 #include <Qt3DRender/QCameraLens>
@@ -12,16 +13,21 @@ Q3DView::Q3DView()
 	: Qt3DWindow()
 {
 	Qt3DCore::QEntity *scene = new Qt3DCore::QEntity();
-	Qt3DCore::QEntity *subEntity = new Qt3DCore::QEntity(scene);
+	Qt3DCore::QEntity *armEntity = new Qt3DCore::QEntity(scene);
 
 	Qt3DRender::QMaterial *material = new Qt3DExtras::QPhongMaterial(scene);
 
-	QUrl yodaData = QUrl::fromLocalFile("C:/Users/alexa/Documents/Arduino/RoboticArm/assets/Mini-Sub.stl");
-	Qt3DRender::QMesh *subMesh = new Qt3DRender::QMesh;
-	subMesh->setMeshName("subMesh");
-	subMesh->setSource(yodaData);
-	subEntity->addComponent(subMesh);
-	subEntity->addComponent(material);
+	QUrl armData = QUrl::fromLocalFile("C:/Users/alexa/Documents/Arduino/RoboticArm/assets/Waist.stl");
+	Qt3DRender::QMesh *armMesh = new Qt3DRender::QMesh;
+	armMesh->setMeshName("subMesh");
+	armMesh->setSource(armData);
+
+	Qt3DCore::QTransform *armTransform = new Qt3DCore::QTransform;
+	armTransform->setScale3D(QVector3D(0.10, 0.10, 0.10));
+
+	armEntity->addComponent(armMesh);
+	armEntity->addComponent(material);
+	armEntity->addComponent(armTransform);
 
 	Qt3DRender::QCamera *camera = this->camera();
 	camera->lens()->setPerspectiveProjection(45.0f, 16.0f / 9.0f, 0.1f, 1000.0f);
