43d32802968376265d88e759373269052df53721
Alexandre Desgagné <alexandre1998@live.ca>
2020-04-02 18:53:16 -0400

    Control and record buttons.

--- RoboticArm/QControlPanel.cpp
+++ RoboticArm/QControlPanel.cpp
@@ -6,15 +6,20 @@
 #include <QFormLayout>
 
 const QString QControlPanel::sBoxTitle{  QString("Control") };
+const QString QControlPanel::mControlButtonTitle{ QString("Control") };
+const QString QControlPanel::mRecordButtonTitle{ QString("Record movements") };
 
 QControlPanel::QControlPanel(QWidget *parent)
 	: QGroupBox(parent),
+	mState{ State::control },
 	mWaistSlider{ new QSlider },
 	mShoulderSlider{ new QSlider },
 	mElbowSlider{ new QSlider },
 	mWristRollSlider{ new QSlider },
 	mWristPitchSlider{ new QSlider },
-	mGripperSlider{ new QSlider }
+	mGripperSlider{ new QSlider },
+	mControlButton{ new QPushButton(mControlButtonTitle) },
+	mRecordButton{ new QPushButton(mRecordButtonTitle) }
 {
 	setTitle(sBoxTitle);
 	QFormLayout * inputLayout{ new QFormLayout };
@@ -24,7 +29,14 @@ QControlPanel::QControlPanel(QWidget *parent)
 	inputLayout->addRow("Elbow", sliderLayout(mElbowSlider, 0, 180));
 	inputLayout->addRow("Shoulder", sliderLayout(mShoulderSlider, 0, 180));
 	inputLayout->addRow("Base rotation", sliderLayout(mWaistSlider, 0, 180));
+	inputLayout->addRow(mControlButton);
+	inputLayout->addRow(mRecordButton);
 	setLayout(inputLayout);
+
+	updateControls();
+
+	connect(mControlButton, &QPushButton::pressed, this, &QControlPanel::control);
+	connect(mRecordButton, &QPushButton::pressed, this, &QControlPanel::record);
 }
 
 QControlPanel::~QControlPanel()
@@ -50,4 +62,27 @@ QHBoxLayout * QControlPanel::sliderLayout(QSlider * & slider, int min, int max)
 	layout->addWidget(label);
 
 	return layout;
+}
+
+void QControlPanel::control() {
+	mState = State::control;
+	updateControls();
+}
+
+void QControlPanel::record() {
+	mState = State::record;
+	updateControls();
+}
+
+void QControlPanel::updateControls() 
+{
+	mControlButton->setEnabled(mState != State::control);
+	mRecordButton->setEnabled(mState != State::record);
+
+	mWaistSlider->setEnabled(mState != State::record);
+	mShoulderSlider->setEnabled(mState != State::record);
+	mElbowSlider->setEnabled(mState != State::record);
+	mWristRollSlider->setEnabled(mState != State::record);
+	mWristPitchSlider->setEnabled(mState != State::record);
+	mGripperSlider->setEnabled(mState != State::record);
 }
--- RoboticArm/QControlPanel.h
+++ RoboticArm/QControlPanel.h
@@ -3,8 +3,10 @@
 #include <QHBoxLayout>
 #include <QGroupBox>
 #include <QString>
+#include <QPushButton>
 
 class QSlider;
+class QPushButton;
 
 class QControlPanel : public QGroupBox
 {
@@ -14,7 +16,12 @@ public:
 	QControlPanel(QWidget *parent);
 	~QControlPanel();
 
+	enum class State { control = 0, record = 1 };
+	State state() const { return mState; }
+
 private:
+	State mState;
+
 	QSlider * mWaistSlider;
 	QSlider * mShoulderSlider;
 	QSlider * mElbowSlider;
@@ -22,7 +29,15 @@ private:
 	QSlider * mWristPitchSlider;
 	QSlider * mGripperSlider;
 
+	QPushButton * mControlButton;
+	QPushButton * mRecordButton;
+
 	QHBoxLayout * sliderLayout(QSlider * & sb, int min, int max);
+	void control();
+	void record();
+	void updateControls();
 
 	static const QString sBoxTitle;
+	static const QString mControlButtonTitle;
+	static const QString mRecordButtonTitle;
 };
